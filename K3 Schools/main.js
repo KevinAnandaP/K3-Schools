96b1e642eca7ae87ad0a456425840af73ae7847b
Kevin Ananda Putra <122136284+KevinAnandaP@users.noreply.github.com>
2023-01-08 22:30:53 +0700

    K3 Schools

---
+++ K3 Schools/main.js
@@ -0,0 +1,13 @@
+var navLinks = document.getElementById("navLinks");
+function showMenu()
+{
+    navLinks.style.right = "0";
+}
+function hideMenu()
+{
+    navLinks.style.right = "-200px";
+}
+function igKepin()
+{
+    window.location.assign("https://www.instagram.com/kevinanp_/?hl=id")
+}
